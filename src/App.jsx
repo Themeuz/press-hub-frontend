672134154bb3faa7b3e091c3d7ed22d27dd54184
Themeuz <matheusbicegodata@gmail.com>
2026-03-05 23:23:11 -0300

    feat: adicionado nova stack, configuração react, vite e js

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950 text-zinc-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Press Hub - Início, agora vai!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,5 @@
+export function Sidebar() {
+  return (
+    <aside className="w-64 bg-zinc-900 border-r border-zinc-800 p-6 flex flex-col"></aside>
+  );
+}
